b48bae57bc40e6da3af8bc7ea99b75a91abf8359
deleau nassim <nassim@MacBook-Air-de-deleau.local>
2019-11-02 17:15:32 +0100

    Ajout de la class Client avec commentaire

--- client.cpp
+++ client.cpp
@@ -13,26 +13,30 @@ Client::Client(string ID , string nom , string prenom , int reservation){
 	m_reservation = reservation;
 }
 
+// Renvoi le nom du client
 string Client::getNom(){
 
 	return m_nom;
 }
 
+// Renvoi le prenom du client
 string Client::getPrenom(){
 
 	return m_prenom;
 }
 
+//Renvoi l'ID du client
 string Client::getID(){
 
 	return m_ID;
 }
 
+//Renvoi le numero de reservation du client 
 int Client::getReservation(){
 
 	return m_reservation;
 }
-
+//Permet de modifier les informations du client
 void Client::setClient( string ID, string nom , string prenom , int reservation){
 
 	m_ID=ID;
